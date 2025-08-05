3c83ff32f70f429993ce4eb12f0f68e0ff165bfe
arshi <arshiga246@gmail.com>
2025-08-05 11:01:30 +0530

    folder structure added

---
+++ src/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative-AI-Project",
+    version="0.1.0",
+    author="Arshiga",
+    author_email="arshiga246@gmail.com.com",
+    packages=find_packages(),
+    install_requires=[]
+)
